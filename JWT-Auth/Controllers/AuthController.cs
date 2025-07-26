0e300879ea63b850aca7df57de04eead9c30ab61
sivavithu <sivavithu15@live.com>
2025-07-26 09:48:23 +0530

    Add project files.

---
+++ JWT-Auth/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using JWT_Auth.Enitities;
+ // Corrected typo: 'Enitities' -> 'Entities'
+using JWT_Auth.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new(); // This is okay for minimal testing
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            var hashedPassword = new PasswordHasher<User>()
+                                    .HashPassword(user, request.Password);
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+
+            return Ok(user);
+        }
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not Found");
+            }
+
+            var result = new PasswordHasher<User>()
+                            .VerifyHashedPassword(user, user.PasswordHash, request.Password);
+
+            if (result == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong Password");
+            }
+
+            string token = "success";
+
+            return Ok(token);
+        }
+
+
+    }
+}
---
+++ JWT-Auth/Enitities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace JWT_Auth.Enitities
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
